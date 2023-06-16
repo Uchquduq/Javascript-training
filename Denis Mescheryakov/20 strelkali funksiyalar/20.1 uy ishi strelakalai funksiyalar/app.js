2e03476b9a4e78467b9fa3f2d9c4b7792c495d73
Rufat <rufatxasanov1998@gmail.com>
2023-06-16 19:17:22 +0500

    strelkali funksiyalar

---
+++ Denis Mescheryakov/20 strelkali funksiyalar/20.1 uy ishi strelakalai funksiyalar/app.js
@@ -0,0 +1,42 @@
+//1 Переделать функцию с использованием функции-стрелки (в методе reduce тоже использовать arrow function):
+
+// function sum() {
+// const params = Array.prototype.slice.call(arguments);
+
+// if (!params.length) return 0;
+
+// return params.reduce(function (prev, next) { return prev + next; });
+// }
+
+// sum(1, 2, 3, 4); // 10
+// sum(); // 0
+
+// что такое метод reduce можно прочитать https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce
+
+const sum = (...params) => {
+  if (!params.length) return 0;
+  return params.reduce((prev, next) => prev + next);
+}
+
+
+//2 Переделать функцию с использованием функции-стрелки
+
+// function convertToObject(num) {
+
+//   const obj = {
+
+//       value: num,
+
+//       isOdd: Boolean(num % 2)
+
+//   }
+
+//   return obj;
+
+// }
+
+
+const convertToObject = (num) => ({
+  value: num,
+  isOdd: Boolean(num % 2)
+});
